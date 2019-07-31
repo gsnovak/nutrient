d2e2828d77c8cf4906e2b8dc369e0bf861f36fa4
Graham <gnovak.sh@gmail.com>
2019-07-30 22:19:10 -0500

    updated link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import Navbar from 'react-bootstrap/Navbar';
 const Footer = () => {
     return (
         <footer bg="dark" className="App-footer">
-            API: <a href="url"> https://ndb.nal.usda.gov/ndb/search/list</a>
+            <a href="https://ndb.nal.usda.gov/ndb/search/list"> API Reference</a>
         </footer>
     );
 }
